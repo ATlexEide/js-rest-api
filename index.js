13a3c1ff194779719ed0de7bdb9721cf7e3d9aa1
Alexander Eide <atlexeide@gmail.com>
2024-10-10 14:25:26 +0200

    Add class constructor, POST and GET

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+class Message {
+  constructor(content) {
+    this.message = content;
+  }
+  getContent() {
+    return this.message;
+  }
+}
 app.listen(3000, () => {
   console.log("Running on port 3000");
 });
@@ -12,6 +20,7 @@ app.get("/message", (req, res, next) => {
 app.use(express.json());
 
 app.post("/message", (req, res, next) => {
-  const message = req.body.message;
-  res.json({ recievedMessage: message });
+  const newMessage = new Message(req.body.message);
+  console.log(newMessage.getContent());
+  res.json({ "recievedMessage": newMessage.getContent() });
 });
